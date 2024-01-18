0eaa043f447c43fd863109064deb1b47e6b85fe1
無免ライダー <106133707+mumenrider21@users.noreply.github.com>
2024-01-18 11:35:52 +0900

    Update main.js

--- topics/topic14/main.js
+++ topics/topic14/main.js
@@ -1,101 +1,46 @@
-//画像の設定
+let localStream;
 
-var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0;
-    if (windowwidth > 768){
-      var responsiveImage = [//PC用の画像
-        { src: 'https://m.media-amazon.com/images/I/71c0+fcDMWL.jpg'},
-        { src: 'https://jfv.jp/wp-content/uploads/2022/05/paaaaa_background.gif'},
-        { src: 'https://steamuserimages-a.akamaihd.net/ugc/937194633965961867/EF7D9713618CF86471801A4E8B24527FEDD799B7/?imw=1024&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false'},
-        { src: 'https://cdn.piapro.jp/thumb_i/vo/vohbs9lre4q5dc39_20200702111327_0740_0500.gif'}
-
-      ];
-    } else {
-      var responsiveImage = [//タブレットサイズ（768px）以下用の画像
-        { src: 'https://i.ytimg.com/vi/2e9BwQDbJQI/maxresdefault.jpg' },
-        { src: 'https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/6-1-3/img/img_sp_02.jpg' },
-        { src: 'https://coco-factory.jp/ugokuweb/wp-content/themes/ugokuweb/data/6-1-3/img/img_sp_03.jpg' }
-      ];
-    }
-
-//Vegas全体の設定
-
-$('#slider').vegas({
-    overlay: true,//画像の上に網線やドットのオーバーレイパターン画像を指定。
-    transition: 'blur',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
-    transitionDuration: 1000,//切り替わりのアニメーション時間をミリ秒単位で設定
-    delay: 10000,//スライド間の遅延をミリ秒単位で。
-    animationDuration: 20000,//スライドアニメーション時間をミリ秒単位で設定
-    animation: 'kenburns',//スライドアニメーションの種類。http://vegas.jaysalvat.com/documentation/transitions/参照。kenburns、kenburnsUp、kenburnsDown、kenburnsRight、kenburnsLeft、kenburnsUpLeft、kenburnsUpRight、kenburnsDownLeft、kenburnsDownRight、randomが設定可能。
-    slides: responsiveImage,//画像設定を読む
-  });
-
-  var slider;
-var sliderFlag = false;
-var breakpoint = 768;//768px以下の場合
-  
-function sliderSet() {
-        var windowWidth = window.innerWidth;
-        if (windowWidth >= breakpoint && !sliderFlag) {//768px以上は1行でスライダー表示
-            slider = $('.slider').bxSlider({
-            touchEnabled:false,//リンクを有効にするためスライドをマウスでドラッグした際にスライドの切り替えを可能にする機能を無効化
-      mode: 'vertical',//縦スライド指定
-      controls: false,//前後のコントロールを表示させない。
-      auto: 'true',//自動的にスライド
-      pager: false//ページ送り無効化
-    });
-            sliderFlag = true;
-        } else if (windowWidth < breakpoint && sliderFlag) {
-            slider.destroySlider();//bxSliderのOptionであるdestroySliderを使用してスライダーの動きを除去
-            sliderFlag = false;
-        }
-    }
-
-$(window).on('load resize', function() {
-        sliderSet();
+// カメラ映像取得
+navigator.mediaDevices.getUserMedia({video: true, audio: true})
+  .then( stream => {
+  // 成功時にvideo要素にカメラ映像をセットし、再生
+  const videoElm = document.getElementById('my-video')
+  videoElm.srcObject = stream;
+  videoElm.play();
+  // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
+  localStream = stream;
+}).catch( error => {
+  // 失敗時にはエラーログを出力
+  console.error('mediaDevice.getUserMedia() error:', error);
+  return;
 });
 
+const peer = new Peer({
+  key: '06796348-ff21-4524-9899-6b561225f4e9',
+  debug: 3
+});
 
+peer.on('open', () => {
+  document.getElementById('my-id').textContent = peer.id;
+});
 
-$(window).on('load',function(){ //画面遷移時にギャラリーの画像が被らないように、すべての読み込みが終わった後に実行する
+// 発信処理
+document.getElementById('make-call').onclick = () => {
+  const theirID = document.getElementById('their-id').value;
+  const mediaConnection = peer.call(theirID, localStream);
+  setEventListener(mediaConnection);
+};
 
-  //＝＝＝Muuriギャラリープラグイン設定
-  var grid = new Muuri('.grid', {
-  
-  //アイテムの表示速度※オプション。入れなくても動作します
-  showDuration: 600,
-  showEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-  hideDuration: 600,
-  hideEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-    
-  // アイテムの表示/非表示状態のスタイル※オプション。入れなくても動作します
-    visibleStyles: {
-      opacity: '1',
-      transform: 'scale(1)'
-    },
-    hiddenStyles: {
-      opacity: '0',
-      transform: 'scale(0.5)'
-    }    
+// イベントリスナを設置する関数
+const setEventListener = mediaConnection => {
+  mediaConnection.on('stream', stream => {
+    // video要素にカメラ映像をセットして再生
+    const videoElm = document.getElementById('their-video')
+    videoElm.srcObject = stream;
+    videoElm.play();
   });
-  
-  //＝＝＝並び替えボタン設定
-  $('.sort-btn li').on('click',function(){      //並び替えボタンをクリックしたら
-    $(".sort-btn .active").removeClass("active"); //並び替えボタンに付与されているactiveクラスを全て取り除き
-    var className = $(this).attr("class");      //クラス名を取得
-    className = className.split(' ');       //「sortXX active」のクラス名を分割して配列にする
-    $("."+className[0]).addClass("active");     //並び替えボタンに付与されているクラス名とギャラリー内のリストのクラス名が同じボタンにactiveクラスを付与
-    if(className[0] == "sort00"){           //クラス名がsort00（全て）のボタンの場合は、
-       grid.show('');               //全ての要素を出す
-    }else{                      //それ以外の場合は
-      grid.filter("."+className[0]);        //フィルターを実行
-    }
-  });
-  
-  //＝＝＝ Fancyboxの設定
-  $('[data-fancybox]').fancybox({
-   thumbs: {
-      autoStart: true, //グループのサムネイル一覧をデフォルトで出す。不必要であればfalseに
-    },  
-  });
-    
-  });
+}
+peer.on('call', mediaConnection => {
+  mediaConnection.answer(localStream);
+  setEventListener(mediaConnection);
+});
